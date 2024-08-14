7058615f8eb51f0790dc7e2cd8ec6224967e02fb
Amey3010 <46645877+Amey3010@users.noreply.github.com>
2024-08-14 15:35:56 +0530

    Update findIntersection.py

--- coderbytes/findIntersection.py
+++ coderbytes/findIntersection.py
@@ -1,3 +1,5 @@
+#Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false
+
 import re
 def FindIntersection(strArr):
   d1 = [int(i) for i in re.split(",\s",strArr[0])]
