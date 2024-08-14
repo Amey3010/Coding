56d5c812f2b3fa74f167ef6707f2e30f8a10b627
Amey3010 <46645877+Amey3010@users.noreply.github.com>
2024-08-14 13:12:04 +0530

    Update solution.py

--- coderbytes/Codeland Username/solution.py
+++ coderbytes/Codeland Username/solution.py
@@ -2,7 +2,7 @@ import re
 def CodelandUsernameValidation(strParam):
   # code goes here
   #return (4<len(strParam)<25) and (re.match("[a-zA-Z]",strParam[0])) and (re.match("[a-zA-Z0-9_]",strParam)) and not (re.match("[_]",strParam[-1]))
-  return re.search("^[a-zA-Z]\w{5,25}\B[\w][^_]$",strParam) != None
+  return re.search("^[A-Za-z][A-Za-z0-9_]{2,23}[A-Za-z0-9]$",strParam) != None
 
 # keep this function call here 
 print(CodelandUsernameValidation(input()))
